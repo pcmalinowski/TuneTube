f3d215e0c5931d0fdad16aa81d92495102abf65b
pcmalinowski <pcmalinowski@gmail.com>
2017-10-31 19:45:53 -0500

    janky grid-fix

--- index.js
+++ index.js
@@ -13,25 +13,24 @@ $.getJSON(YOUTUBE_SEARCH_URL, data, callback);
 }
 
 function renderResult(result) {
-  // console.log(result.id.videoId);
   return `
   <div class="col-3">
       <div class="entry-video">
-        <iframe src="https://www.youtube.com/embed?listType=search&list=${result.id.videoId}" frameborder="0" gesture="media" allowfullscreen alt="${result.snippet.title}"></iframe>
+        <iframe width="854" height="480" src="https://www.youtube.com/embed?listType=search&list=${result.id.videoId}" frameborder="0" gesture="media" allowfullscreen alt="${result.snippet.title}"></iframe>
       </div>
       <div class="entry info">
         <p class="video-title">${result.snippet.title}</p>
+        <p class="video-description">${result.snippet.description}
       <div>
    </div>
   `;
 }
 
-//        <p class="video-description">${result.snippet.description}
+//        
 
 function displaySearchResults(data) {
 
     const results = data.items.map((item, index) => renderResult(item));
-  console.log(data);
   $('.js-results').html(results);
   $('.js-results-counter').html(`<p>On youtube you would have to dig through ${data.pageInfo.totalResults} results!</p>`);
 }
